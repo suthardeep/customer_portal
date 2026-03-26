f7cfa28b4d58030cf49ed1ef652da2cad76b981d
meetdevlope <meetjpatel2412@gmail.com>
2026-03-27 02:36:05 +0530

    addded nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath, URL } from "url";
+import { nitro } from "nitro/vite";
 
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
@@ -24,6 +25,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro(),
   ],
   server: {
     proxy: {
